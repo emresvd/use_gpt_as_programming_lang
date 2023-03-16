c53bd0777a11b1a1a17c7c03e037dc4523e3abe6
emresvd <109899306+emresvd@users.noreply.github.com>
2023-03-16 13:29:54 +0300

    ugptapl

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-import openai
-import dotenv
-import os
+import ugptapl
 import sys
 
-dotenv.load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
 path = sys.argv[-1]
-prompt = "translate the above algorithm to python code and write only python code"
 
 with open(path, "r") as f:
-    prompt = f.read() + prompt
+    code = f.read()
 
-completion = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt=prompt,
-    max_tokens=1024,
-    n=1,
-    stop=None,
-    temperature=0.5,
-)
-
-python_code = completion.choices[0].text
-print(python_code)
+u = ugptapl.UGPTapl(code)
+u.translate_to_python_code()
+u.run()
