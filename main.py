3423e0e3a1470db5104e76e274b904e4da30a21d
emresvd <109899306+emresvd@users.noreply.github.com>
2023-03-16 16:21:18 +0300

    --save

--- main.py
+++ main.py
@@ -12,4 +12,8 @@ u.translate_to_python_code()
 if "--print" in sys.argv:
     print(u.python_code)
 
+if "--save" in sys.argv:
+    with open(path + ".py", "w") as f:
+        f.write(u.python_code)
+
 u.run()
